36ad3303d58412a4c288e7325cdb301d0bd96e04
kiboiko <kirboy2013@gmail.com>
2024-10-06 17:51:22 +0300

    version1

--- hello.pas
+++ hello.pas
@@ -1,1 +1,8 @@
-﻿
+﻿begin
+  var n := ReadlnInteger('Введит номер свого языка: 1-Russian,2-English,3-Italiano');
+  case n of
+    1:Println('Привет');
+    2:Println('Hello');
+    3:Println('Ciao');
+  end;
+end.
